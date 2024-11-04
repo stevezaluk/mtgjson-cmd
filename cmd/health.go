d6209d3058466e28df32847eb8f661661e86a317
Steven A. Zaluk <stevezaluk@gmail.com>
2024-11-04 10:00:09 -0500

    Implemented support for calling the /health endpoint with the health command

--- cmd/health.go
+++ cmd/health.go
@@ -2,8 +2,9 @@ package cmd
 
 import (
 	"fmt"
-
 	"github.com/spf13/cobra"
+	"github.com/stevezaluk/mtgjson-sdk-client/health"
+	"os"
 )
 
 var healthCmd = &cobra.Command{
@@ -11,7 +12,17 @@ var healthCmd = &cobra.Command{
 	Short: "Make a call to the /health endpoint to determine the health of the server",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("health called")
+		result, err := health.GetHealth()
+		if err != nil {
+			fmt.Println("[error]", err.Error())
+			os.Exit(0)
+		}
+
+		if result {
+			fmt.Println("[success] Server is healthy")
+		} else {
+			fmt.Println("[error] Server is not healthy or offline")
+		}
 	},
 }
 
