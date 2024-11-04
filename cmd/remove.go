690df29d9538e645137a2b6e564fb6752ed5785c
Steven A. Zaluk <stevezaluk@gmail.com>
2024-11-04 09:08:53 -0500

    Implemented pre-run valiadation for the number of args

--- cmd/remove.go
+++ cmd/remove.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
 	deck_model "github.com/stevezaluk/mtgjson-models/deck"
 )
@@ -11,7 +13,11 @@ var removeCmd = &cobra.Command{
 	Short: "Remove a card from a deck or a set",
 	Long:  ``,
 	PreRun: func(cmd *cobra.Command, args []string) {
-		fmt.Println("pre-remove called")
+		if len(args) != 3 {
+			fmt.Println("[error] Invalid number of arguments for add command")
+			fmt.Println("[error] You must pass a type, code, uuid (and board if a deck)")
+			os.Exit(0)
+		}
 	},
 	Run: func(cmd *cobra.Command, args []string) {
 	},
