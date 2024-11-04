5aedbf70b9b6398ecab505822fcae555fc2dc776
Steven A. Zaluk <stevezaluk@gmail.com>
2024-11-04 00:25:47 -0500

    Initial commit for the delete command. This will allow us to delete cards, sets, or decks

---
+++ cmd/delete.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var deleteCmd = &cobra.Command{
+	Use:   "delete",
+	Short: "Delete a set, deck, or card based on a code/UUID",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("delete called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(deleteCmd)
+}
