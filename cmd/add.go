9d71a7dbdf9d02d9cb56945bc6c1eee41f7938dc
Steven A. Zaluk <stevezaluk@gmail.com>
2024-11-04 09:05:45 -0500

    Implemented functionaltiy for adding cards to a deck

--- cmd/add.go
+++ cmd/add.go
@@ -5,6 +5,9 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	deck_model "github.com/stevezaluk/mtgjson-models/deck"
+	"github.com/stevezaluk/mtgjson-models/errors"
+	"github.com/stevezaluk/mtgjson-sdk-client/deck"
 )
 
 var addCmd = &cobra.Command{
@@ -12,17 +15,46 @@ var addCmd = &cobra.Command{
 	Short: "Add a card to a set or deck",
 	Long:  ``,
 	PreRun: func(cmd *cobra.Command, args []string) {
-		if len(args) == 4 {
+		if len(args) != 3 {
 			fmt.Println("[error] Invalid number of arguments for add command")
 			fmt.Println("[error] You must pass a type, code, uuid (and board if a deck)")
 			os.Exit(0)
 		}
 	},
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("add called")
+		passedType := args[0]
+
+		if passedType == "set" {
+			fmt.Println("[error] Not implemented yet")
+			os.Exit(0)
+		} else if passedType == "deck" {
+			code := args[1]
+			cards := []string{args[2]}
+
+			board, _ := cmd.Flags().GetString("board")
+			invalid, noExist, err := deck.AddCards(code, cards, board)
+			if err != nil {
+				fmt.Println("[error]", err.Error())
+				if err == errors.ErrDeckUpdateFailed {
+
+					invalids := map[string][]string{"Invalid Cards:": invalid, "No Exist: ": noExist}
+					for key, value := range invalids {
+						fmt.Println(key)
+						for _, val := range value {
+							fmt.Println(val)
+						}
+					}
+
+				}
+			}
+		} else {
+			fmt.Println("[error] Invalid type for add command. Can be: set, deck")
+			os.Exit(0)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(addCmd)
+	addCmd.PersistentFlags().StringP("board", "b", deck_model.MAINBOARD, "Specify the board you want to add to if working with a deck")
 }
