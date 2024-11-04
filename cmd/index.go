2743bfdcc427f8369b1ba02d61fb6e3625dfcc45
Steven A. Zaluk <stevezaluk@gmail.com>
2024-11-03 22:43:52 -0500

    Implemented boiler plate for the index command's logic

--- cmd/index.go
+++ cmd/index.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -10,8 +11,26 @@ var indexCmd = &cobra.Command{
 	Use:   "index",
 	Short: "List all items of a specific type. Can be deck, card, or set",
 	Long:  ``,
+	PreRun: func(cmd *cobra.Command, args []string) {
+		if len(args) != 1 {
+			fmt.Println("[error] A type must be passed to the index command. Can be: deck, card, or set")
+			os.Exit(0)
+		}
+	},
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("index called")
+		passedType := args[0]
+
+		if passedType == "deck" {
+
+		} else if passedType == "card" {
+
+		} else if passedType == "set" {
+			fmt.Println("[error] Not implemented yet")
+			os.Exit(0)
+		} else {
+			fmt.Println("[error] Invalid type for index command: ", passedType)
+			os.Exit(0)
+		}
 	},
 }
 
