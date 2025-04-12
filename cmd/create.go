4299dd57b3b17799e2d61679f0606d3ff66586b7
Steven A. Zaluk <stevezaluk@gmail.com>
2025-04-12 13:22:23 -0400

    Implemented functionallity for the card command

--- cmd/create.go
+++ cmd/create.go
@@ -16,7 +16,13 @@ limitations under the License.
 package cmd
 
 import (
+	"fmt"
+	"github.com/gofrs/uuid/v5"
+
 	"github.com/spf13/viper"
+	"github.com/stevezaluk/mtgjson-models/card"
+	"github.com/stevezaluk/mtgjson-models/meta"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -30,9 +36,55 @@ var createCmd = &cobra.Command{
 	},
 }
 
+var createCardCmd = &cobra.Command{
+	Use:   "card",
+	Short: "Create a new card",
+	Long: `At a minimum a card name, and an ID must be passed. If an owner is not passed, one will be
+created under the logged in user's account'`,
+	PreRun: func(cmd *cobra.Command, args []string) {
+		if len(args) != 1 {
+			fmt.Println("error: 1 argument is required to create a card. A name must be passed")
+			os.Exit(1)
+		}
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		var mtgjsonv4id string
+		if len(args) == 1 {
+			fmt.Println("warn: A MTGJSONv4ID was not passed for the card. One will be generated for you")
+			namespace, err := uuid.NewV4()
+			if err != nil {
+				fmt.Println("error: Failed to generate UUID for card (", err.Error(), ")")
+				os.Exit(1)
+			}
+
+			mtgjsonv4id = uuid.NewV5(namespace, args[0]).String()
+		} else {
+			mtgjsonv4id = args[1]
+		}
+
+		newCard := &card.CardSet{
+			Name: args[0],
+			Identifiers: &meta.CardIdentifiers{
+				MtgjsonV4Id: mtgjsonv4id,
+			},
+		}
+
+		owner, _ := cmd.Flags().GetString("owner")
+		message, err := mtgjson.Card.NewCard(newCard, owner)
+		if err != nil {
+			fmt.Println("error: Failed to create card (", err.Error(), ")")
+			fmt.Println("Message from API: ", message.Message)
+			os.Exit(1)
+		}
+
+		fmt.Println("Created:", args[0])
+	},
+}
+
 func init() {
 	createCmd.PersistentFlags().String("owner", "system", "Set the owner to use for the query. Defaults to the email provided in the config file")
 	viper.BindPFlag("api.owner", createCmd.PersistentFlags().Lookup("owner"))
 
+	createCmd.AddCommand(createCardCmd)
 	rootCmd.AddCommand(createCmd)
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/stevezaluk/mtgjson-cmd
 go 1.23.2
 
 require (
-	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.8.1
 	github.com/spf13/viper v1.19.0
+	github.com/stevezaluk/mtgjson-models v1.3.3
 	github.com/stevezaluk/mtgjson-sdk-client v1.1.8
 )
 
@@ -13,9 +13,11 @@ require (
 	github.com/auth0/go-auth0 v1.13.1 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/go-resty/resty/v2 v2.16.2 // indirect
+	github.com/gofrs/uuid/v5 v5.3.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
@@ -24,7 +26,6 @@ require (
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/stevezaluk/mtgjson-models v1.3.3 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
